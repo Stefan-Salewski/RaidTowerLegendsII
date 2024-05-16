27945fa023380c698b7deb3f18e2ac516d033b2c
Stefan <stefansalewski91@gmail.com>
2024-05-16 14:23:10 -0400

    changed main

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
 
+pygame.font.init()
+my_font = pygame.font.SysFont('Comic Sans MS', 30)
+
+pygame.display.set_caption('Raid Tower Legends II')
+
 while running:
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
@@ -15,6 +20,8 @@ while running:
 
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("purple")
+    text_surface = my_font.render(str(clock), False, (0, 0, 0))
+    screen.blit(text_surface, (0, 0))
 
     # RENDER YOUR GAME HERE
 
@@ -22,5 +29,6 @@ while running:
     pygame.display.flip()
 
     clock.tick(60)  # limits FPS to 60
+    print(clock)
 
 pygame.quit()
