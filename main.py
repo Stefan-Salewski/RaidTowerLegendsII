937761841c9a0897a7b3dd95bd82c5dd93cc6f6c
Darth-Shady <shadidtabeeb@gmail.com>
2024-05-20 19:53:40 -0400

    formatting changes to main additional function added in menu; settings_screen

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 clock = pygame.time.Clock()
 fps = 60
 running = True
+game_state = "menu" # default state is menu
 
 # colors
 RED = (255, 0, 0)
@@ -27,7 +28,7 @@ BLUE = (0, 0, 255)
 GREEN = (0, 255, 0)
 colours = [WHITE, BLACK, RED, BLUE, GREEN]
 
-# text
+# fonts
 pygame.font.init()
 REGULAR_FONT = pygame.font.Font('Born2bSportyFS.otf', 30)
 TITLE_FONT = pygame.font.Font("Born2bSportyFS.otf", 70)
@@ -43,19 +44,15 @@ quit_img_hover = pygame.image.load("quit_select.png").convert_alpha()
 start = Button(SCREEN_WIDTH * 0.5, SCREEN_HEIGHT * 0.5, start_img, start_img_hover, 0.7)
 quit = Button(SCREEN_WIDTH * 0.5, SCREEN_HEIGHT * 0.8, quit_img, quit_img_hover,0.7)
 
-# game
+# game  levels
 level_generator = level_generation(pygame, screen, SCREEN_WIDTH, SCREEN_HEIGHT)
 roomlist = []
 
-# Player initialization, move this to menu so that it only happens at start of game
+# Player initialization
 player_entity = Player(SCREEN_WIDTH,SCREEN_HEIGHT) # dimensions are automatically halved in the function
 
-# state variable
-game_state = "menu"
-
-pygame.mixer.init()
-
 # Load the menu music
+pygame.mixer.init()
 pygame.mixer.music.load("Menu - Spaceship Hangar.wav")
 pygame.mixer.music.play(-1)  # Play the music in a loop
 
