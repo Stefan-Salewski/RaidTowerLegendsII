199fdd51a67952511606328ab2a8f1f53c7afa12
Darth-Shady <shadidtabeeb@gmail.com>
2024-05-20 13:36:23 -0400

    made better looking more functional buttons positioned them differently, hover textures and they work now

--- Buttons.py
+++ Buttons.py
@@ -7,22 +7,37 @@ class Button():
         # locking aspect ratio
         image_width = image.get_width()
         image_height = image.get_height()
+
+        image_hover_width = hover_image.get_width()
+        image_hover_height = hover_image.get_height()
         # creating image scaled to scale
         self.image = pygame.transform.scale(image,(int(image_width * scale), int(image_height * scale)))
+        self.image_hover = pygame.transform.scale(hover_image,(int(image_hover_width * scale), int(image_hover_height * scale)))
         # turning image into rect object
-        self.rect = image.get_rect()
+        self.rect = self.image.get_rect()
         # location of rect centre
         self.rect.center = (x,y)
         self.clicked = False
-    def draw(self,screen_instance):
-        # draw button, centre at x,y
-        screen_instance.blit(self.image,(self.rect.x,self.rect.y))
+
+    def draw(self, screen_instance):
+        # get mouse position
+        pos = pygame.mouse.get_pos()
+
+        # check if mouse is hovering over the button
+        if self.rect.collidepoint(pos):
+            # draw the hover image
+            screen_instance.blit(self.image_hover, (self.rect.x, self.rect.y))
+        else:
+            # draw the normal image
+            screen_instance.blit(self.image, (self.rect.x, self.rect.y))
+
     def function(self):
         # get mouse position
         pos = pygame.mouse.get_pos()
 
         # checking if the cursor is colliding with rect obj.
         if (self.rect.collidepoint(pos)):
+
 
             # Checking if left click is happening, and  only registering it if it's a new click
             # since we don't want continuous holding of the left click button to count as multiple clicks
