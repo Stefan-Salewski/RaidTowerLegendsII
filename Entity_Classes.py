86bf136c2a20f7bfcc5b999547286e466daa453f
Darth-Shady <shadidtabeeb@gmail.com>
2024-06-09 21:00:44 -0400

    Added comments to Entity_Classes.py

--- Entity_Classes.py
+++ Entity_Classes.py
@@ -1,10 +1,9 @@
 import time
-
 import pygame
 import random
 import camera
 
-#COLOURS
+# COLOURS
 RED = (255, 0, 0)
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
@@ -15,9 +14,10 @@ PURPLE = (128, 0, 128)
 colours = [WHITE, BLACK, RED, BLUE, GREEN, RANDOM, PURPLE]
 
 
+# base entity class
 class Entity():
     def __init__(self, invulnerability, health, damage, cancollide, game_state):
-        #invulnerability should be a true/false toggle, walls should be true for example
+        # invulnerability should be a true/false toggle, walls should be true for example
         self.invulnerability = invulnerability
         self.max_health = health
         self.health = self.max_health
@@ -27,6 +27,7 @@ class Entity():
         self.offset = pygame.math.Vector2()
 
         print("Entity spawning")
+
     def Take_Damage(self, damage):
         self.health = self.health - damage
         if self.health <= 0:
@@ -44,13 +45,15 @@ class Entity():
         self.offset.y = rect.centery - half_h
 
         # convert rect to a surface and draw to the screen
-        if(colorfill):
+        if colorfill:
             surface.fill(colours[5])
         screen_instance.blit(surface, rect.topleft - self.offset)
 
 
+# player class
 class Player(Entity):
-    def __init__(self, SCREEN_WIDTH, SCREEN_HEIGHT, bullet_img, bullets, game_state, invulnerability=False, health=100, damage=25,firerate = 1, cancollide=True, ):
+    def __init__(self, SCREEN_WIDTH, SCREEN_HEIGHT, bullet_img, bullets, game_state, invulnerability=False, health=100,
+                 damage=25, firerate=1, cancollide=True):
         self.bullet_img = bullet_img
         self.money = 100
         self.bullets = bullets
@@ -71,8 +74,9 @@ class Player(Entity):
         self.player_moving = pygame.math.Vector2()
         self.mousepos = pygame.math.Vector2()
         print("Player initialized")
+
     def player_input(self, screen_instance, deltaTime):
-        #pygame.draw.rect(screen_instance, colours[5], self.player)
+        # pygame.draw.rect(screen_instance, colours[5], self.player)
         self.mousepos = pygame.mouse.get_pos()
 
         key = pygame.key.get_pressed()
@@ -88,56 +92,60 @@ class Player(Entity):
             self.player_moving.x -= 1
         if (key[pygame.K_d]) or (key[pygame.K_RIGHT]):
             self.player_moving.x += 1
-        # Use unit vectors to set directions and get consistent speed with diagonal and non-diagonal movement
+        # use unit vectors to set directions and get consistent speed with diagonal and non-diagonal movement
         if (self.player_moving.length() > 0):  # if there's movement basically.
             self.player_moving = self.player_moving.normalize() * self.player_speed  # multiplying unit vector by speed
 
         self.movement(self.surface, self.rect, self.player_moving, screen_instance, True)
 
-        #calculating the world position of the mouse, then getting the direction vector between the mouse and player
+        # calculating the world position of the mouse, then getting the direction vector between the mouse and player
         world_mouse_pos = self.mousepos + self.offset
         shoot_dir = world_mouse_pos - self.rect.center
         # drawing a line to test if the direction vector is correct
         pygame.draw.line(screen_instance, colours[5], self.rect.center - self.offset, world_mouse_pos - self.offset)
-        #shooting behaviour
+        # shooting behaviour
         if pygame.mouse.get_pressed()[0] and self.firecooldown <= 0:
-              # Normalize and scale by bullet speed
+            # normalize and scale by bullet speed
             self.shoot_bullets(shoot_dir, screen_instance, 5, self.damage, self.amount)
             self.firecooldown = self.firerate
 
         self.firecooldown -= 1 * deltaTime
 
     def shoot_bullets(self, movement_vector, screen_instance, speed, damage, amount):
-        #normalizing and multiplying by speed to get the right vector
-        offset_range = 50  # Example: max offset is 10% of the original vector length
+        # normalizing and multiplying by speed to get the right vector
+        offset_range = 50  # example: max offset is 10% of the original vector length
         if amount == 1:
             offset_range = 0
         for _ in range(amount):
-            # Apply a slight random offset to the movement vector
+            # apply a slight random offset to the movement vector
             offset_x = random.uniform(-offset_range, offset_range)
             offset_y = random.uniform(-offset_range, offset_range)
 
-            # Create a new movement vector with the offset
+            # create a new movement vector with the offset
             offset_movement_vector = movement_vector + pygame.Vector2(offset_x, offset_y)
             offset_movement_vector = offset_movement_vector.normalize() * speed
 
-            # Create and append the new bullet
-            new_bullet = Bullet(damage, speed, offset_movement_vector, screen_instance, self.bullet_img, self.rect.centerx, self.rect.centery)
+            # create and append the new bullet
+            new_bullet = Bullet(damage, speed, offset_movement_vector, screen_instance, self.bullet_img,
+                                self.rect.centerx, self.rect.centery)
             self.bullets.append(new_bullet)
 
     def power_up(self):
         pass
 
+
+# bullet class
 class Bullet(Entity):
-    def __init__(self, damage, speed, movement_vector, screen_instance,image, x, y):
+    def __init__(self, damage, speed, movement_vector, screen_instance, image, x, y):
         self.damage = damage
         self.speed = speed
-        self.movement_vector = pygame.math.Vector2(movement_vector).normalize() * self.speed  # Normalize and apply speed
+        self.movement_vector = pygame.math.Vector2(
+            movement_vector).normalize() * self.speed  # normalize and apply speed
         self.position = pygame.math.Vector2(x, y)
         self.screen_instance = screen_instance
         self.offset = pygame.math.Vector2()
         self.sprite = image
-        #self.surface = pygame.Surface(self.sprite.get_size())
+        # self.surface = pygame.Surface(self.sprite.get_size())
 
         image_width = self.sprite.get_width()
         image_height = self.sprite.get_height()
@@ -150,17 +158,19 @@ class Bullet(Entity):
     def Update(self, main_camera):
         self.position += self.movement_vector
 
-        # Calculate the screen position based on the camera's position
+        # calculate the screen position based on the camera's position
         screen_x = self.position.x - main_camera.offset[0]
         screen_y = self.position.y - main_camera.offset[1]
 
-        # Update the rect for collision purposes
+        # update the rect for collision purposes
         self.rect.x = self.position.x
         self.rect.y = self.position.y
 
-        # Draw the bullet at the calculated screen position
+        # draw the bullet at the calculated screen position
         self.screen_instance.blit(self.image, (screen_x, screen_y))
 
+
+# enemy class
 class Enemy(Entity):
     def __init__(self, health, damage, speed, cancollide, x, y, width, height):
         self.invulnerability = True
@@ -181,20 +191,24 @@ class Enemy(Entity):
         self.enemy_moving = pygame.math.Vector2()
 
     def enemy_movement(self, screen_instance, player_ref, camera_offset):
-        self.enemy_moving = pygame.math.Vector2(player_ref.rect.centerx - self.rect.centerx, (player_ref.rect.centery) - self.rect.centery)
+        self.enemy_moving = pygame.math.Vector2(player_ref.rect.centerx - self.rect.centerx,
+                                                (player_ref.rect.centery) - self.rect.centery)
 
-        # Use unit vectors to set directions and get consistent speed with diagonal and non-diagonal movement
-        if(self.enemy_moving.length() <= 1000 and self.enemy_moving.length() > 0):
+        # use unit vectors to set directions and get consistent speed with diagonal and non-diagonal movement
+        if self.enemy_moving.length() <= 1000 and self.enemy_moving.length() > 0:
             self.enemy_moving = self.enemy_moving.normalize() * self.enemy_speed
-            self.rect.move_ip(self.enemy_moving.x, self.enemy_moving.y)  # moving it by whatever the new vectors coordinates are
+            self.rect.move_ip(self.enemy_moving.x,
+                              self.enemy_moving.y)  # moving it by whatever the new vectors coordinates are
 
-        #convert rect to a surface and draw to the screen
+        # convert rect to a surface and draw to the screen
         self.surface.fill(colours[2])
         offset = self.rect.topleft - camera_offset
         screen_instance.blit(self.surface, offset)
 
         return self.enemy_moving
 
+
+# level end class
 class Level_End(Entity):
     def __init__(self, x, y, width, height):
         self.invulnerability = True
@@ -206,6 +220,9 @@ class Level_End(Entity):
         self.surface = pygame.Surface(self.rect.size)
         self.surface.fill(colours[6])
         self.mask = pygame.mask.from_surface(self.surface)
+
+
+# wall class
 class Wall(Entity):
     def __init__(self, x, y, width, height):
         self.invulnerability = True
@@ -222,6 +239,8 @@ class Wall(Entity):
 
     pass
 
+
+# chest class
 class Chest(Entity):
     def __init__(self, x, y, scale, cost, chest_closed, chest_open, powerups):
         self.damageimage = pygame.image.load("Art/damage.png").convert_alpha()
@@ -238,7 +257,8 @@ class Chest(Entity):
         self.chest_open = chest_open
         image_width = self.chest_closed.get_width()
         image_height = self.chest_closed.get_height()
-        self.image = pygame.transform.scale(self.chest_closed, (int(image_width * self.scale), int(image_height * self.scale)))
+        self.image = pygame.transform.scale(self.chest_closed,
+                                            (int(image_width * self.scale), int(image_height * self.scale)))
         self.mask = pygame.mask.from_surface(self.image)
         self.rect = self.image.get_rect()
         self.rect.x = x
@@ -247,7 +267,7 @@ class Chest(Entity):
     def open_chest(self):
         self.opened = True
         self.image = pygame.transform.scale(self.chest_open, (int(16 * self.scale), int(16 * self.scale)))
-        randomnum = random.randint(0,3)
+        randomnum = random.randint(0, 3)
         if randomnum == 0:  # health
             poweruptoadd = "health"
             powerupimage = self.healthimage
@@ -260,22 +280,24 @@ class Chest(Entity):
         elif randomnum == 3:  # attackspeed
             poweruptoadd = "attackspeed"
             powerupimage = self.attackspeedimage
-        elif randomnum == 4: #multishot
+        elif randomnum == 4:  # multishot
             poweruptoadd = "multishot"
             powerupimage = self.multishotimage
-        newpowerup = Powerup(self.rect.centerx, self.rect.centery - 100, self.scale, powerupimage,poweruptoadd)
+        newpowerup = Powerup(self.rect.centerx, self.rect.centery - 100, self.scale, powerupimage, poweruptoadd)
         self.powerups.append(newpowerup)
 
+
+# powerup class
 class Powerup(Entity):
     def __init__(self, x, y, scale, powerupimage, powerup):
-
         self.invulnerability = True
         self.scale = scale
         self.powerupimage = powerupimage
         self.powerup = powerup
         image_width = self.powerupimage.get_width()
         image_height = self.powerupimage.get_height()
-        self.image = pygame.transform.scale(self.powerupimage, (int(image_width * self.scale), int(image_height * self.scale)))
+        self.image = pygame.transform.scale(self.powerupimage,
+                                            (int(image_width * self.scale), int(image_height * self.scale)))
         self.mask = pygame.mask.from_surface(self.image)
         self.rect = self.image.get_rect()
         self.rect.x = x
